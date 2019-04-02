9f26c7308915008154fdcba00e4c48e13b1a254d
Your Name <you@example.com>
2019-04-02 19:35:28 +0800

    score

--- app/src/main/java/swufe/cc/www/myapplication/ScoreActivity.java
+++ app/src/main/java/swufe/cc/www/myapplication/ScoreActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 public class ScoreActivity extends AppCompatActivity {
 
     TextView score;
+    TextView score2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,24 +18,45 @@ public class ScoreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_score);
 
         score = (TextView) findViewById(R.id.score);
+        score2 = (TextView) findViewById(R.id.score2);
     }
 
     public void btnAdd1(View btn) {
-        showScore(1);
+        if(btn.getId()==R.id.btn_1){
+            showScore(1);
+        }else{
+            showScore2(1);
+        }
+
     }
     public void btnAdd2(View btn) {
-        showScore(2);
+        if(btn.getId()==R.id.btn_2){
+            showScore(2);
+        }else{
+            showScore2(2);
+        }
     }
     public void btnAdd3(View btn) {
-        showScore(3);
+        if(btn.getId()==R.id.btn_3){
+            showScore(3);
+        }else{
+            showScore2(3);
+        }
     }
     public void btnReset(View btn) {
         score.setText("0");
+        score2.setText("0");
     }
     private void showScore(int inc){
         Log.i("show","int="+inc);
         String oldScore = (String)score.getText();
         int newScore = Integer.parseInt(oldScore)+inc;
         score.setText(""+newScore);
+    }
+    private void showScore2(int inc){
+        Log.i("show2","int="+inc);
+        String oldScore = (String)score2.getText();
+        int newScore = Integer.parseInt(oldScore)+inc;
+        score2.setText(""+newScore);
     }
 }
