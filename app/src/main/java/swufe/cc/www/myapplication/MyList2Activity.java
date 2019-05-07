51ae09a6319f80a41f73e011532e72aab6313b8c
Your Name <you@example.com>
2019-05-07 20:53:46 +0800

    week10_from teacher

---
+++ app/src/main/java/swufe/cc/www/myapplication/MyList2Activity.java
@@ -0,0 +1,145 @@
+package swufe.cc.www.myapplication;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MyList2Activity extends ListActivity implements Runnable, AdapterView.OnItemClickListener {
+
+
+    Handler handler;
+    private ArrayList<HashMap<String,String>> listItems;//存放文字、图片信息
+    private SimpleAdapter listItemAdapter;//适配器
+    private final String TAG = "MyList2";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initListView();
+
+        //MyAdapter myAdapter = new MyAdapter(this,R.layout.list_item,listItems);
+        //this.setListAdapter(myAdapter);
+        this.setListAdapter(listItemAdapter);
+
+        Thread t = new Thread(this);
+        t.start();
+
+        handler = new Handler(){
+            public void handleMessage(Message msg){
+                if(msg.what==7){
+                    List<HashMap<String,String>> list2 = (List<HashMap<String,String>>)msg.obj;
+                    listItemAdapter = new SimpleAdapter(MyList2Activity.this,list2,//listItems 数据源
+                            R.layout.list_item,//listItem的XML布局实现
+                            new String[]{"ItemTitle","ItemDetail"},
+                            new int[]{R.id.itemTitle,R.id.itemDetail }//确定数据与布局位置的对应关系
+                    );
+                    setListAdapter(listItemAdapter);
+                }
+                super.handleMessage(msg);
+            }
+        };
+        getListView().setOnItemClickListener(this);
+    }
+    private void initListView(){
+        listItems = new ArrayList<HashMap<String,String>>();
+        for (int i = 0; i < 10; i++) {
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put("ItemTitle", "Rate:" + i);//标题文字
+            map.put("ItemDetail", "datail" + i);//详情描述
+            listItems.add(map);
+        }
+        //生成适配器的 Item 和动态数组对应的元素
+        listItemAdapter = new SimpleAdapter(this,listItems,//listItems 数据源
+                R.layout.list_item,//listItem的XML布局实现
+                new String[]{"ItemTitle","ItemDetail"},
+                new int[]{R.id.itemTitle,R.id.itemDetail }//确定数据与布局位置的对应关系
+                 );
+    }
+
+    @Override
+    public void run() {
+        //获取网络数据，放入list带回主线程
+        List<HashMap<String,String>> retList = new ArrayList<HashMap<String, String>>();
+        Document doc = null;
+        try {
+            Thread.sleep(1000);
+            doc = Jsoup.connect("http://www.boc.cn/sourcedb/whpj/").get();
+            Log.i(TAG,"run:"+doc.title());
+            Elements tables = doc.getElementsByTag("table");
+
+            Element table2 = tables.get(1);
+            //获取TD中的数据
+            Elements tds = table2.getElementsByTag("td");
+            for(int i = 0;i<tds.size();i+=8){
+                Element td1 = tds.get(i);
+                Element td2 = tds.get(i+5);
+
+                String str1 = td1.text();
+                String val = td2.text();
+
+                Log.i(TAG, "run:"+str1+"==>"+val);
+                HashMap<String,String> map = new HashMap<String,String>();
+                map.put("ItemTitle",str1);
+                map.put("ItemDetail",val);
+                retList.add(map);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Message msg = handler.obtainMessage(7);
+        //msg.obj = "Hello from run()";
+        msg.obj = retList;
+        handler.sendMessage(msg);
+
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Log.i(TAG, "onItemClick: parent="+parent);
+        Log.i(TAG, "onItemClick: view="+view);
+        Log.i(TAG, "onItemClick: position="+position);
+        Log.i(TAG, "onItemClick: id="+id);
+
+        HashMap<String,String> map = (HashMap<String,String>)getListView().getItemAtPosition(position);
+        String titleStr = map.get("ItemTitle");
+        String detailStr = map.get("ItemDetail");
+        Log.i(TAG, "onItemClick: titleStr="+titleStr);
+        Log.i(TAG, "onItemClick: detailStr="+detailStr);
+
+        TextView title = (TextView) view.findViewById(R.id.itemTitle);
+        TextView detail = (TextView) view.findViewById(R.id.itemDetail);
+        String title2 = String.valueOf(title.getText());
+        String detail2 = String.valueOf(detail.getText());
+        Log.i(TAG, "onItemClick: title2="+title2);
+        Log.i(TAG, "onItemClick: detail2="+detail2);
+
+        //打开新的页面，传入参数
+        Intent rateCalc = new Intent(this,RateCalcActivity.class);
+        rateCalc.putExtra("title",titleStr);
+        rateCalc.putExtra("rate",Float.parseFloat(detailStr));
+        startActivity(rateCalc);
+    }
+}
