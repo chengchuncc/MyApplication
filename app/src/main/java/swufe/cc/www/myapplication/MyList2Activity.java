022b230735bdd29bce9d7a452e8661d4e539a315
Your Name <you@example.com>
2019-05-14 20:33:41 +0800

    week11_from teacher

--- app/src/main/java/swufe/cc/www/myapplication/MyList2Activity.java
+++ app/src/main/java/swufe/cc/www/myapplication/MyList2Activity.java
@@ -1,6 +1,8 @@
 package swufe.cc.www.myapplication;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -21,11 +23,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class MyList2Activity extends ListActivity implements Runnable, AdapterView.OnItemClickListener {
+public class MyList2Activity extends ListActivity implements Runnable, AdapterView.OnItemClickListener,AdapterView.OnItemLongClickListener {
 
 
     Handler handler;
-    private ArrayList<HashMap<String,String>> listItems;//存放文字、图片信息
+    private List<HashMap<String,String>> listItems;//存放文字、图片信息
     private SimpleAdapter listItemAdapter;//适配器
     private final String TAG = "MyList2";
 
@@ -44,8 +46,8 @@ public class MyList2Activity extends ListActivity implements Runnable, AdapterVi
         handler = new Handler(){
             public void handleMessage(Message msg){
                 if(msg.what==7){
-                    List<HashMap<String,String>> list2 = (List<HashMap<String,String>>)msg.obj;
-                    listItemAdapter = new SimpleAdapter(MyList2Activity.this,list2,//listItems 数据源
+                    listItems = (List<HashMap<String,String>>)msg.obj;
+                    listItemAdapter = new SimpleAdapter(MyList2Activity.this,listItems,//listItems 数据源
                             R.layout.list_item,//listItem的XML布局实现
                             new String[]{"ItemTitle","ItemDetail"},
                             new int[]{R.id.itemTitle,R.id.itemDetail }//确定数据与布局位置的对应关系
@@ -56,6 +58,7 @@ public class MyList2Activity extends ListActivity implements Runnable, AdapterVi
             }
         };
         getListView().setOnItemClickListener(this);
+        getListView().setOnItemLongClickListener(this);
     }
     private void initListView(){
         listItems = new ArrayList<HashMap<String,String>>();
@@ -141,5 +144,31 @@ public class MyList2Activity extends ListActivity implements Runnable, AdapterVi
         rateCalc.putExtra("title",titleStr);
         rateCalc.putExtra("rate",Float.parseFloat(detailStr));
         startActivity(rateCalc);
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+        Log.i(TAG, "onItemLongClick: 长按列表项position="+ position);
+        //删除操作
+        //listItems.remove(position);
+        //listItemAdapter.notifyDataSetChanged();
+        //构造对话框进行确定操作
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("提示").setMessage("请确认是否删除当前数据").setPositiveButton("是", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.i(TAG, "onClick: 对话框事件处理");
+                listItems.remove(position);
+                listItemAdapter.notifyDataSetChanged();
+            }
+        }).setNegativeButton("否",null);
+
+        builder.create().show();
+
+
+        Log.i(TAG, "onItemLongClick: size="+ listItems.size());
+
+        return true;
     }
 }
--- app/src/main/java/swufe/cc/www/myapplication/MyListActivity.java
+++ app/src/main/java/swufe/cc/www/myapplication/MyListActivity.java
@@ -2,26 +2,23 @@ package swufe.cc.www.myapplication;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyListActivity extends AppCompatActivity implements Runnable{
+public class MyListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private final String TAG = "MyList";
     Handler handler;
     ListView listView;
+    ArrayAdapter adapter;
+
+    List<String> data = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,60 +26,26 @@ public class MyListActivity extends AppCompatActivity implements Runnable{
         setContentView(R.layout.activity_my_list);
 
         listView = (ListView) findViewById(R.id.mylist);
-        String data[]={"waiting"};
+        //init data
+        for(int i=0;i<10;i++){
+            data.add("item"+i);
+        }
 
-        ListAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
         listView.setAdapter(adapter);
+        listView.setEmptyView(findViewById(R.id.nodata));
+        listView.setOnItemClickListener(this);
 
-        Thread t = new Thread(this);
-        t.start();
 
-        handler = new Handler(){
-            public void handleMessage(Message msg){
-                if(msg.what==7){
-                    List<String> list2 = (List<String>)msg.obj;
-                    ListAdapter adapter = new ArrayAdapter<String>(MyListActivity.this,android.R.layout.simple_list_item_1,list2);
-                    listView.setAdapter(adapter);
-                }
-                super.handleMessage(msg);
-            }
-        };
+
 
     }
 
     @Override
-    public void run() {
-        //获取网络数据，放入list带回主线程
-        List<String> retList = new ArrayList<String>();
-        Document doc = null;
-        try {
-            Thread.sleep(3000);
-            doc = Jsoup.connect("http://www.boc.cn/sourcedb/whpj/").get();
-            Log.i(TAG,"run:"+doc.title());
-            Elements tables = doc.getElementsByTag("table");
-            Element table1 = tables.get(1);
-            Elements tds = table1.getElementsByTag("td");
-            for(int i = 0;i<tds.size();i+=8){
-                Element td1 = tds.get(i);
-                Element td2 = tds.get(i+5);
-
-                String str1 = td1.text();
-                String val = td2.text();
-
-                Log.i(TAG, "run:"+td1.text()+"==>"+td2.text());
-                retList.add(str1+"==>"+val);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        Message msg = handler.obtainMessage(7);
-        //msg.obj = "Hello from run()";
-        msg.obj = retList;
-        handler.sendMessage(msg);
-
+    public void onItemClick(AdapterView<?> listv, View view, int position, long id) {
+        Log.i(TAG, "onItemClick: position="+ position);
+        Log.i(TAG, "onItemClick: parent="+ listv);
+        adapter.remove(listv.getItemAtPosition(position));
+        //adapter.notifyDataSetChanged();
     }
 }
