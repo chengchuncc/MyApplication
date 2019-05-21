7033af14dcb6e35163726af3457e5cb37b9f5f15
Your Name <you@example.com>
2019-05-21 20:35:12 +0800

    week12_from teacher--SQLite...

--- app/src/main/java/swufe/cc/www/myapplication/RateActivity.java
+++ app/src/main/java/swufe/cc/www/myapplication/RateActivity.java
@@ -162,9 +162,21 @@ public class RateActivity extends AppCompatActivity implements Runnable{
         if(item.getItemId()==R.id.menu_set){
             openConfig();
         }else if(item.getItemId()==R.id.open_list){
-            Intent list = new Intent(this, MyList2Activity.class);
-
+            //打开列表窗口
+            Intent list = new Intent(this, RateListActivity.class);
             startActivity(list);
+            //测试数据库
+           /* RateItem item1 = new RateItem("aaaa","123");
+            RateManager manager = new RateManager(this);
+            manager.add(item1);
+            manager.add(new RateItem("bbb","23.5"));
+            Log.i(TAG, "onOptionsItemSelected: 写入数据完毕");
+
+            //查询所有数据
+            List<RateItem> testList = manager.listAll();
+            for(RateItem i : testList){
+                Log.i(TAG, "onOptionsItemSelected: 取出数据[id="+i.getId()+"]Name="+i.getCurName()+"Rate="+i.getCurRate());
+            }*/
         }
 
         return super.onOptionsItemSelected(item);
---
+++ app/src/main/java/swufe/cc/www/myapplication/RateItem.java
@@ -0,0 +1,43 @@
+package swufe.cc.www.myapplication;
+
+public class RateItem {
+    private int id;
+    private String curName;
+    private String curRate;
+
+    public RateItem() {
+        super();
+        this.curName = "";
+        this.curRate = "";
+
+    }
+
+    public RateItem(String curName, String curRate) {
+        this.curName = curName;
+        this.curRate = curRate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCurName() {
+        return curName;
+    }
+
+    public void setCurName(String curName) {
+        this.curName = curName;
+    }
+
+    public String getCurRate() {
+        return curRate;
+    }
+
+    public void setCurRate(String curRate) {
+        this.curRate = curRate;
+    }
+}
